9b6a76a521efa343eb07d5db9e3c80e1390d0693
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-12-13 11:28:23 +0200

    test

--- src/js/Application.js
+++ src/js/Application.js
@@ -15,7 +15,22 @@ export default class Application extends EventEmitter {
   }
   setEmojis(emojis) {
     this.emojis = emojis;
+<<<<<<< Updated upstream
   }
 
   addBananas() {}
+=======
+    const div = document.querySelector("#emojis");
+    div.innerHTML = "";
+    this.emojis.forEach(emoji => {
+      const emojiP = document.createElement("p");
+      emojiP.innerText = emoji;
+      div.appendChild(emojiP);
+    })
+  }
+
+  addBananas() {
+    this.emojis = this.emojis.map(emoji => emoji + this.banana);
+  }
+>>>>>>> Stashed changes
 }
